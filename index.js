b820fecded510d727b1ff2e9a419491ffd724f2b
arunthampi <arun.thampi@gmail.com>
2016-01-26 10:37:33 -0800

    Export more useful functions

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var Robot = require('./src/robot');
 var Response = require('./src/response');
 var _ref = require('./src/message'), Message = _ref.Message, TextMessage = _ref.TextMessage, CatchAllMessage = _ref.CatchAllMessage;
+var _ref1 = require('./src/adapter'), Adapter = _ref1.Adapter, NestorAdapter = _ref1.NestorAdapter;
+var Brain = require('./src/brain');
+var User = require('./src/user');
 
 module.exports = {
+  Adapter: Adapter,
+  NestorAdapter: NestorAdapter,
+  Brain: Brain,
   Robot: Robot,
   Response: Response,
   Message: Message,
   TextMessage: TextMessage,
   CatchAllMessage: CatchAllMessage,
+  User: User
 }
